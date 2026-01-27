65f76e19934dd1e32798db823f7a253de590e5e4
awsrmmustansarjavaid <aws.rmmustansarjavaid@gmail.com>
2026-01-28 00:27:49 +0500

    aws

---
+++ CLoud-Engineering/Cloud-research-study-drive/DevOps-research-study-drive/Cloud-ISPs-AWS-AZ-GC/AWS-Cloud-Engineering/AWS-Cloud-Practice-dev/AWS-Labs-AWS-Labs-Guide/AWS-Labs-Projects/AWS-Basic-Labs-Projects/AWS Challenge Lab: Build a Dynamic Café Website (Dev + Prod)/AWS Dynamic Cafe Website Fully Project/☕ AWS CAFE — Front & Backend Code Script/☕ AWS CAFE — JS Backend Code Script/config.js
@@ -0,0 +1,14 @@
+/* ======================================
+   GLOBAL CONFIGURATION
+   Charlie Café HR System
+====================================== */
+
+const CONFIG = {
+    APP_NAME: "Charlie Café HR System",
+    API_BASE: "https://<API-ID>.execute-api.us-east-1.amazonaws.com/prod",
+
+    COGNITO: {
+        USER_POOL_ID: "us-east-1_XXXXXX",
+        CLIENT_ID: "XXXXXXXXXXXX"
+    }
+};
